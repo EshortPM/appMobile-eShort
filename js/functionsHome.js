6cfb56c19bf2a0b7627fd39ad052227cd53747b0
PabloSoligo <pablosoligo@gmail.com>
2013-11-29 08:21:40 +0100

    29/11/2013 08:21

--- js/functionsHome.js
+++ js/functionsHome.js
@@ -22,8 +22,25 @@ function onDeviceReady() {
 		useCachedDialogs: false
 	});
 	
-	FB.Event.subscribe('auth.statusChange', handleStatusChange);
-	FB.getLoginStatus(handleStatusChange);
+	/*FB.Event.subscribe('auth.statusChange', handleStatusChange);*/
+	FB.getLoginStatus(function(response){
+		if (response.authResponse) {
+			showAlert('ESTA LOGUEADO EN FACEBOOK');
+			if (response1.status === 'connected') {
+				showAlert('ESTA REGISTRADO EN LA APP');
+				$(".btnLoginFB").hide();	
+			} else if (response1.status === 'not_authorized') {
+				showAlert('NO HA DADO LOS PERMISOS');
+				$(".btnLoginFB").show();
+			} else {				
+				showAlert('NO ESTA LOGUEADO EN FACEBOOK');
+				$(".btnLoginFB").show();		
+			}
+		} else {
+			showAlert('NO ESTA LOGUEADO EN FACEBOOK');
+			$(".btnLoginFB").show();
+		}
+	});
 	
 	
 	
@@ -32,11 +49,14 @@ function onDeviceReady() {
 			FB.login(function(response){
 				FB.getLoginStatus(function(response1) {
 					if (response1.status === 'connected') {
-						showAlert('ESTA LOGUEADO EN FACEBOOK');			
+						showAlert('ESTA REGISTRADO EN LA APP');
+						$(".btnLoginFB").hide();			
 					} else if (response1.status === 'not_authorized') {
-						showAlert('ESTA LOGUEADO EN FACEBOOK SIN PERMISOS');
+						showAlert('NO HA DADO LOS PERMISOS');
+						$(".btnLoginFB").show();
 					} else {				
-						showAlert('NO ESTA LOGUEADO EN FACEBOOK');			
+						showAlert('NO ESTA LOGUEADO EN FACEBOOK');
+						$(".btnLoginFB").show();			
 					}
 				}, true);		
 			},{scope: 'email'});
@@ -49,11 +69,3 @@ function onDeviceReady() {
 }
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ DEVICE READY */
 function showAlert(text){navigator.notification.alert(text,null,nombreApp,txt_btn_aceptar);}
-
-function handleStatusChange(response) {
-	if (response.authResponse) {
-		showAlert('ESTA LOGUEADO EN FACEBOOK');
-	} else {
-		showAlert('NO ESTA LOGUEADO EN FACEBOOK');
-	}
-}
